7b57cf8e3a7ed843bace30ced58b009ce2acf207
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2024-11-02 21:38:27 +0100

    -

---
+++ PJC_01/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "fmt/base.h"
+
+
+int main() {
+    fmt::println("hello");
+    return 0;
+}
---
+++ PJC_02/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "PJC_01 file" << std::endl;
+    return 0;
+}
